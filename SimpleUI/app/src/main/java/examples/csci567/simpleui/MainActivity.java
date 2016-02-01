cbf65bec77596449ad4e076f90d568a3686af7c0
priyanka <pchordia@mail.csuchico.edu>
2016-02-01 00:51:43 -0800

    Simple User Interface

---
+++ SimpleUI/app/src/main/java/examples/csci567/simpleui/MainActivity.java
@@ -0,0 +1,107 @@
+package examples.csci567.simpleui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        Button Button1 = (Button) findViewById(R.id.button1);
+        Button Button2 = (Button) findViewById(R.id.button2);
+        Button Button3 = (Button) findViewById(R.id.button3);
+
+
+
+        Button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                TextView textView1 = (TextView) findViewById(R.id.textView1);
+                textView1.setTextSize(8);
+            }
+        });
+
+
+
+
+        Button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView2 = (TextView) findViewById(R.id.textView2);
+                textView2.setTextSize(24);
+
+            }
+
+        });
+
+        Button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView1 = (TextView) findViewById(R.id.textView1);
+                TextView textView2 = (TextView) findViewById(R.id.textView2);
+                textView1.setTextSize(14);
+                textView2.setTextSize(14);
+
+            }});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+}
+
+
+
